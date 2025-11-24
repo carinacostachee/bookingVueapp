a68eb4dad1afb4a7bce4a5adf4379039a7508415
Carina Costache <carinacostache@gmail.com>
2025-11-24 11:44:00 +0100

    chore: made progress on the main page added tables, created dataset in Firebase

---
+++ bookingapp/src/components/Header.vue
@@ -0,0 +1,9 @@
+<script setup>
+import '@/assets/header.css'
+</script>
+
+<template>
+  <header>
+    <h1 class="title">CC's Fine Dining</h1>
+  </header>
+</template>
---
+++ bookingapp/src/components/HeroSection.vue
@@ -0,0 +1,28 @@
+<script setup>
+import '@/assets/hero.css'
+import tableRestImage from '@/components/icons/table.png'
+</script>
+
+<template>
+  <div class="hero-section">
+    <h1 class="titlepage">Current tables in the restaurant:</h1>
+    <div class="tables">
+      <div class="tableId">
+        <img :src="tableRestImage" alt="Restaurant table" />
+        <h2 class="tableName">Table</h2>
+      </div>
+      <div class="tableId">
+        <img :src="tableRestImage" alt="Restaurant table" />
+        <h2 class="tableName">Table</h2>
+      </div>
+      <div class="tableId">
+        <img :src="tableRestImage" alt="Restaurant table" />
+        <h2 class="tableName">Table</h2>
+      </div>
+      <div class="tableId">
+        <img :src="tableRestImage" alt="Restaurant table" />
+        <h2 class="tableName">Table</h2>
+      </div>
+    </div>
+  </div>
+</template>
